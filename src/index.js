2bf874df00fc0a10f7442ac78b0e8b1355115511
“kostik700015” <kostik700015@gmail.com>
2023-10-30 23:04:27 -0400

    incremen, decrement onclick functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { type } from '@testing-library/user-event/dist/type';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
 );
 
