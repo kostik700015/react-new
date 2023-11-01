94122b16a08641f67990835e1d8e315ecea50fb2
“kostik700015” <kostik700015@gmail.com>
2023-10-31 23:12:57 -0400

    connected states

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 
 function App() {
  const [likes, setLikes] = useState(0)
+ const [value, setValue] = useState("Text within input")
+  
   function increment(){
     setLikes(likes + 1)
   }
@@ -14,6 +16,8 @@ function App() {
     <div className="App">
       <h1>It's Alive!</h1>
       <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input type="text" value={value} onChange={event => setValue(event.target.value)}></input>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
