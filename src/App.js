68abb65997b5dd4a0aba412a7ccf123bfa51fc3c
“kostik700015” <kostik700015@gmail.com>
2023-11-01 23:41:03 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Counter from './components/Counter';
 import Input from './components/Input';
+import PostItem from './components/PostItem';
+import './styles/App.css';
 
 
 function App() {
@@ -8,8 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>It's Alive!</h1>
-      <Counter/>
-      <Input/>
+      <PostItem post={{id:1, title: 'Jsvascript', body: 'Body of post'}}/>
     </div>
   );
 }
