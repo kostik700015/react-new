ba753bb9e94dcf6f355d9a99bf0e1f46935ac177
“kostik700015” <kostik700015@gmail.com>
2023-11-16 23:11:22 -0500

    MyButton and MyInput were added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Input from './components/Input';
 import PostItem from './components/PostItem';
 import './styles/App.css';
 import PostList from './components/PostList';
+import MyButton from './UI/button/MyButton';
+import MyInput from './UI/input/MyInput';
 
 
 function App() {
@@ -13,8 +15,23 @@ function App() {
   {id:2, title: 'Jsvascript 2', body: 'Body of post'},
   {id:3, title: 'Jsvascript 3', body: 'Body of post'},
 ])
+const [title, setTitle] = useState('')
+const addNewPost = (e) =>{
+  e.preventDefault()
+  console.log(title)
+}
   return (
     <div className="App">
+    <form>
+      <MyInput
+       value={title}
+       onChange={e => setTitle(e.target.value)}
+       type="text" 
+       placeholder='Title of post'
+       />
+      <MyInput type="text" placeholder='Post description'/>
+      <MyButton onClick={addNewPost} >Create post</MyButton>
+    </form>
     <PostList posts={posts} title="List of posts"/>
     </div>
   );
